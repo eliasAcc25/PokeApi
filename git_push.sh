2f024cd96ead5af4b547be8e6434945b94ce9b51
Elias <eliasandregabriel2018@gmail.com>
2025-10-06 16:58:09 -0300

    ready

---
+++ git_push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd "C:\Users\elias.andre.torres\Downloads\pokeApi\pokeApi"
+git add .
+git commit -m "docs: adiciona diagrama arquitetural draw.io e atualiza README"
+git push origin main
+
